75c19279e8bfc6bcb022a456b20cbe48b1d89485
Balthazar Gronon <bgronon@gmail.com>
2018-09-10 17:46:49 +0200

    Add GA tracking

--- src/client.js
+++ src/client.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactGA from 'react-ga'
 import { hydrate, render } from 'react-dom'
 import { ConnectedRouter } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
@@ -13,5 +14,21 @@ const store = createStore(history, window.__INITIAL_STATE__)
 const r = __PROD__ ? hydrate : render
 const rootNode = document.getElementById('root')
 const rootComponent = <App store={store} Router={ConnectedRouter} routerProps={{ history }} />
+
+if (__PROD__) {
+  ReactGA.initialize('UA-41961567-3')
+}
+
+const track = () => {
+  if (!__PROD__) {
+    return
+  }
+
+  const { pathname, search } = window.location
+  ReactGA.pageview(`${pathname}${search}`)
+}
+
+history.listen(track)
+track()
 
 r(rootComponent, rootNode)
