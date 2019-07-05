6651d364bfd5ebc410d36e1a625fd70e305138aa
Balthazar Gronon <bgronon@gmail.com>
2019-07-05 13:24:51 -0700

    Update news

--- src/components/News.js
+++ src/components/News.js
@@ -41,6 +41,30 @@ class News extends Component {
           6 juillet – 8 septembre 2019
           <br />
           <br />
+          <img src="/assets/carton-obscura.png" width={600} />
+          <br />
+          <br />
+          Exposition monographique
+          <br />
+          <em>Cent visages</em>
+          <br />
+          Dessin et photographie
+          <br />
+          Musée Joseph Déchelette
+          <br />
+          42300 - Roanne
+          <br />
+          9 novembre 2019 – 11 février 2020
+          <br />
+          <br />
+          <a
+            href="http://www.museederoanne.fr/expositions/exposition-a-venir-2091.html"
+            target="_blank"
+          >
+            Site du Musée
+          </a>
+          <br />
+          <br />
           <em>Suites et portraits</em>
           <br />
           Dessin et photographie
